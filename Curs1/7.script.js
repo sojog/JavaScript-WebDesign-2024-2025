33968bc5f99438f6da1f9b9a6511eb131faa4897
Silviu Ojog <sil.ojog@gmail.com>
2025-05-27 19:48:10 +0300

    Curs 1 - JavaScript

---
+++ Curs1/7.script.js
@@ -0,0 +1,27 @@
+
+console.log("Acest log vine dintr-un fisier separat")
+var x = 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+x = x + 100
+console.log(x)
+
